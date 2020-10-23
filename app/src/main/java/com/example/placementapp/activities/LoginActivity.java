eaf5d13e78cee64564aca30cf44337f5dfbdf72d
Nishant <nishantduttmishra@gmail.com>
2020-10-23 22:53:15 +0530

    aravind lol

--- app/src/main/java/com/example/placementapp/activities/LoginActivity.java
+++ app/src/main/java/com/example/placementapp/activities/LoginActivity.java
@@ -3,13 +3,16 @@ package com.example.placementapp.activities;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import com.example.placementapp.R;
 import com.example.placementapp.helper.FirebaseHelper;
+import com.example.placementapp.helper.GMailSender;
 import com.example.placementapp.utils.StringUtils;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -25,6 +28,32 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     public String username;
     public String password;
 
+    private class MyTask extends AsyncTask<Void, Void, Void> {
+        String result;
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            GMailSender sender = new GMailSender("nishantduttmishra@gmail.com", "9709848722");
+            try {
+                sender.sendMail("This is Subject",
+                        "This is Body",
+                        "nishantduttmishra@gmail.com",
+                        "nishantd.mishra@gmail.com");
+            } catch (Exception e) {
+                Log.e("SendMail", e.getMessage(), e);
+            }
+
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+
+            super.onPostExecute(aVoid);
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,7 +62,8 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         usernameView = findViewById(R.id.username);
         passwordView = findViewById(R.id.password);
         loginButton.setOnClickListener(this);
-    }
+        new MyTask().execute();
+}
 
     @Override
     public void onClick(View view) {
