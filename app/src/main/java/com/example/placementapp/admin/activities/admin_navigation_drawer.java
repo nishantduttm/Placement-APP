042754ee71e46a5051c078d00e73f43f9e7f617f
Nishant <nishantduttmishra@gmail.com>
2020-10-26 15:06:45 +0530

    gg

---
+++ app/src/main/java/com/example/placementapp/admin/activities/admin_navigation_drawer.java
@@ -0,0 +1,124 @@
+package com.example.placementapp.admin.activities;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.example.placementapp.admin.fragment.*;
+
+import com.example.placementapp.R;
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+
+public class admin_navigation_drawer extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    //FOR DESIGN
+    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+
+    private Fragment fragmentAddPlacementDrive;
+
+
+    private static final int ADD_PLACEMENT_DRIVE_FRAGMENT = 0;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_navigation_drawer);
+
+        // 6 - Configure all views
+
+        this.configureToolBar();
+
+        this.configureDrawerLayout();
+
+        this.configureNavigationView();
+    }
+
+    @Override
+    public void onBackPressed() {
+        // 5 - Handle back click to close menu
+        if (this.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            this.drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+
+        // 4 - Handle Navigation Item Click
+        int id = item.getItemId();
+
+
+        switch (id){
+            case R.id.admin_navigation_drawer_add_placement_drive :
+                toolbar.setTitle("My Account");
+                Toast.makeText(this, "Presses", Toast.LENGTH_SHORT).show();
+                this.showFragment(ADD_PLACEMENT_DRIVE_FRAGMENT);
+                break;
+            case R.id.admin_navigation_drawer_notifications:
+                break;
+            default:
+
+                break;
+        }
+
+        this.drawerLayout.closeDrawer(GravityCompat.START);
+
+        return true;
+    }
+
+    private void showFragment(int fragmentIdentifier){
+        switch (fragmentIdentifier){
+            case ADD_PLACEMENT_DRIVE_FRAGMENT :
+                this.addPlacementDriveFragment();
+                break;
+
+            default:
+                break;
+        }
+    }
+    private void addPlacementDriveFragment(){
+        if (this.fragmentAddPlacementDrive == null) this.fragmentAddPlacementDrive =AddPlacementDriveFragment.newInstance();
+        this.startTransactionFragment(this.fragmentAddPlacementDrive);
+    }
+    // ---------------------
+    // CONFIGURATION
+    // ---------------------
+
+    // 1 - Configure Toolbar
+    private void configureToolBar(){
+        this.toolbar = (Toolbar) findViewById(R.id.activity_admin_main_toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    // 2 - Configure Drawer Layout
+    private void configureDrawerLayout(){
+        this.drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    // 3 - Configure NavigationView
+    private void configureNavigationView(){
+        this.navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+    private void startTransactionFragment(Fragment fragment){
+        if (!fragment.isVisible()){
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.activity_main_frame_layout, fragment).commit();
+        }
+    }
+}
